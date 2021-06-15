3fb3aaca6715becaa22526aefa8eb541b99af10f
Syed <82619273+Syedm-hub@users.noreply.github.com>
2021-06-14 21:30:01 -0500

    updated

--- script.js
+++ script.js
@@ -7,3 +7,15 @@ $(document).ready(function () {
         var currentSrchHist = JSON.parse(localStorage.getItem("prevCityWeatherSrch"));
         renderLastCity(currentSrchHist[0]);
     };
+
+    displaySearchHist();
+
+    //city coordinates 
+
+    $(document).on("click", "prvCity", function(e) {
+        e.preventDefault();
+        var cityName = $(this).attr("id");
+        var apiKey = "6406ca836e96fe35d13d0645f945ad0b";
+        var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&cnt=5&units=imperial&appid=" + apiKey;
+        
+    }
