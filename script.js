65f06a759cd4df6d197e3ed460160dc5b112de92
Syed <82619273+Syedm-hub@users.noreply.github.com>
2021-06-14 22:48:14 -0500

    updated

--- script.js
+++ script.js
@@ -39,4 +39,57 @@ $(document).ready(function () {
         });
     });
 
+    function weatherForecast(results) {
+        $(".hide").attr("class", "row");
+        var currentCityName = results.name;
+        $("currentCityInfo").text(currentCityName + " ");
+        var currentCityLon = results.coord.lon;
+        var currentCityLat = results.coord.lat;
+        findWithCoords(currentCityLat, currentCityLon);
+        var currentCityDt = results.sys.sunrise;
+        dateConverter(currentCityDt);
+        var currentWethIcon = results.weather[0].icon;
+        weatherIcon(currentWethIcon);
+    };
+    
+    function findWithCoords(currentCityCoLat, currentCityCoLon) {
+        var apiKey = "6406ca836e96fe35d13d0645f945ad0b";
+        var queryURL2 = "https://api.openweathermap.org/data/2.5/onecall?lat=" + currentCityCoLat + "&lon=" + currentCityCoLon + "&exclude=minutely,hourly&units=imperial&appid=" + apiKey;
+        $.ajax({
+            url: queryURL2,
+            method: "GET"
+        }).then(function (results) {
+            var currentCityTemp = results.current.temp;
+            $("#currentTemp").text("Temperature: " + currentCityTemp + " \u00B0F");
+            var currentCityHum = results.current.humidity;
+            $("#currentHumid").text("Humidity: " + currentCityHum + "%");
+            var currentCityWinSpeed = results.current.wind_speed;
+            $("#currentWind").text("Wind Speed: " + currentCityWinSpeed + " MPH");
+            var currentCityUvi = results.current.uvi;
+            uviIndexSeverity(currentCityUvi);
+            fiveDayForecast(results);
+        });
+    };
+
+    function uviIndexSeverity(currentCityUvi) {
+        $("#currentUvi").text("");
+        var uviIndexText = $("<span>");
+        uviIndexText.text("UV Index: ");
+        $("#currentUvi").append(uviIndexText);
+        var currentCityUviHolder = $("<span>");
+        if (currentCityUvi >= 0 && currentCityUvi <= 2) {
+            currentCityUviHolder.attr("class", "low-uvi");
+        } else if (currentCityUvi > 2 && currentCityUvi <= 5) {
+            currentCityUviHolder.attr("class", "moderate-uvi");
+        } else if (currentCityUvi > 5 && currentCityUvi <= 7) {
+            currentCityUviHolder.attr("class", "high-uvi");
+        } else if (currentCityUvi > 7 && currentCityUvi <= 10) {
+            currentCityUviHolder.attr("class", "very-high-uvi");
+        } else if (currentCityUvi > 10) {
+            currentCityUviHolder.attr("class", "extreme-uvi forecast-square");
+        };
+        currentCityUviHolder.text(currentCityUvi);
+        $("#currentUvi").append(currentCityUviHolder);
+    };
+
     
